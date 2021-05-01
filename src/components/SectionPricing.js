d2c34b7a61ba9160383c90ccfa3033344e9b0d6b
Stackbit Code Editor <projects@stackbit.com>
2021-05-01 18:15:44 +0000

    SectionPricing.js: changed

--- src/components/SectionPricing.js
+++ src/components/SectionPricing.js
@@ -50,6 +50,7 @@ export default class SectionPricing extends React.Component {
                 </div>
               </div>
               )}
+              h
             </section>
         );
     }
