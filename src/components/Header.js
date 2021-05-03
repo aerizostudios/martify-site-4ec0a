8de5054f2d0c2b4c325b42b291c781f59de55fd4
Stackbit Code Editor <projects@stackbit.com>
2021-05-03 16:21:52 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,15 @@ import Action from './Action';
 export default class Header extends React.Component {
     componentDidUpdate(prevProps) {
   // Typical usage (don't forget to compare props):
-  if (this.props.userID !== prevProps.userID) {
-    this.fetchData(this.props.userID);
-  }
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60902077185beb22b30997a8/1f4pftl4s';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
 }
     render() {
         return (
