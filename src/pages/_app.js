a2d4065c1ef0cdca22887fc3822f2780b7cc64c4
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 11:32:37 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
 
     return <>
     <Head>
-    <!--Start of Tawk.to Script-->
+
 <script type="text/javascript">
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
@@ -49,7 +49,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
-<!--End of Tawk.to Script-->
+
     </Head>
     <Component {...pageProps} />
     </>;
