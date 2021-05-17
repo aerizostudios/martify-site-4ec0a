44234497371cc8b78ea19d873c298aac262e7ec0
Stackbit Code Editor <projects@stackbit.com>
2021-05-17 11:32:24 +0000

    _app.js: changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -35,23 +35,24 @@ function MyApp({ Component, pageProps }) {
         };
     }, []);
 
-    return <>   <Head>
-        <meta name="robots" content="noindex" />
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Martify</title>
-
-        <link rel="manifest" href="/manifest.json" />
-        <link href="/logo16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/logo32.png" rel="icon" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/logo192.png"></link>
-        <meta name=<Component {...pageProps} /></>;
+    return <>
+    <Head>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60902077185beb22b30997a8/1f4pftl4s';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+    </Head>
+    <Component {...pageProps} />
+    </>;
 }
 
 // Only uncomment this method if you have blocking data requirements for
