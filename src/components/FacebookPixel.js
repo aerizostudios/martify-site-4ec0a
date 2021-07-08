7c2addfcacce1dc8fa8a174f886c26a12d82d2fd
Stackbit Code Editor <projects@stackbit.com>
2021-07-08 11:30:12 +0000

    FacebookPixel.js: changed

--- src/components/FacebookPixel.js
+++ src/components/FacebookPixel.js
@@ -4,23 +4,26 @@ export default () => (
   <React.Fragment>
     <script
       dangerouslySetInnerHTML={{
-        __html: `  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js%27);
-  fbq('init', '857128068495836');
-  fbq('track', 'PageView');`
+        __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '225393876096664');
+fbq('track', 'PageView');`
       }}
     />
     <noscript
       dangerouslySetInnerHTML={{
-        __html: `<img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=857128068495836&ev=PageView&noscript=1"
-/>`
+        __html: `<img
+        height="1"
+        width="1"
+        style="display:none"
+        src="https://www.facebook.com/tr?id=225393876096664&ev=PageView&noscript=1"
+      />`
       }}
     />
   </React.Fragment>
