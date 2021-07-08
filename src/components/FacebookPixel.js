d0d27bb9d5ba20bceb4a9c1f377531819875e247
Stackbit Code Editor <projects@stackbit.com>
2021-07-08 07:33:59 +0000

    FacebookPixel.js: changed

--- src/components/FacebookPixel.js
+++ src/components/FacebookPixel.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => 
   <React.Fragment>
-    <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+    {/* <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -15,5 +15,5 @@ fbq('track', 'PageView');` }}
     />
 <noscript><img height="1" width="1" style="display:none"
 src="https://www.facebook.com/tr?id=989093995196442&ev=PageView&noscript=1"
-/></noscript>
+/></noscript> */}
   </React.Fragment>
