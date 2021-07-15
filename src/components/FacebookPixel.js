ee07f3931d5c33b2ace6ce818b44096c27ee4692
Stackbit Code Editor <projects@stackbit.com>
2021-07-15 17:15:18 +0000

    FacebookPixel.js: changed

--- src/components/FacebookPixel.js
+++ src/components/FacebookPixel.js
@@ -10,11 +10,11 @@ export default () => (
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js%27);
-  fbq('init', '857128068495836');
+  fbq('init', '888231618781430');
   fbq('track', 'PageView');`}}
     />
     <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=857128068495836&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=888231618781430&ev=PageView&noscript=1"
 />` }}
     />
   </React.Fragment>
